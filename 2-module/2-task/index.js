c4ee67cb3547842045a0af690c44a55303b81586
Dmitrii Krasnov <slipenber@yandex.ru>
2026-06-04 16:37:08 +0300

    Занятие 2: Объекты

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,11 @@
 function isEmpty(obj) {
-  // ваш код...
+  for (let key in obj){
+    return false;
+  }
+  return true;
 }
+
+let schedule = {4: true};
+console.log(( isEmpty(schedule) )); // true
+schedule["8:30"] = "подъём";
+console.log(( isEmpty(schedule) )); // false
