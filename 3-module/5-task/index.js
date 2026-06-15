a74b286e105c31df96eacf7ddc5e122a82072d1e
Dmitrii Krasnov <slipenber@yandex.ru>
2026-06-15 08:54:38 +0300

    Занятие 3: Функции и массивы

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
+let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+
 function getMinMax(str) {
-  // ваш код...
+  const num = str
+    .split(' ')
+    .map(item => Number(item))
+    .filter(item => !isNaN(item));
+    const min = num.reduce((result, current) => result < current ? result : current)
+    const max = num.reduce((result, current) => result > current ? result : current)
+      return {min, max};
 }
+
+console.log(getMinMax(inputData));
